31c4d4f8e7e7babd653c3ab72d9fd123f020e60e
Krushna Sinnarkar <78172473+krushnasinnarkar@users.noreply.github.com>
2024-07-30 23:56:36 +0530

    Deleting vite.config.ts via script

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-
-export default defineConfig({
-    plugins: [solid()],
-    base: "/FMC-Job-listings-with-filtering/",
-});
